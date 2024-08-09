e2669948ec61fbdd4c46fdf90bd7b4beabce6d6c
NemoAM7 <71778005+NemoAM7@users.noreply.github.com>
2024-08-09 18:47:11 +0530

    extended api-1

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 using API;
+using API.data;
+using API.Data;
 using API.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occured during migration");
+}
 
 app.Run();
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent {
-
+  match () {
+    
+  }
 }
