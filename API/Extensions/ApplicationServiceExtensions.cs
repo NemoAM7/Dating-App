32ab6dc655d1f7ac063143b77d7ffa21d239e1bb
NemoAM7 <71778005+NemoAM7@users.noreply.github.com>
2024-08-16 00:34:43 +0530

    Extending the api - 2

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -12,11 +14,13 @@ public static class ApplicationServiceExtensions
     {
         services.AddControllers();
         services.AddDbContext<DataContext>(opt =>
-        {
+    {
             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
